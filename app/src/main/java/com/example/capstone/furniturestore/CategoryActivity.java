8353c92e012e2f2d5269ead373361cc22f8b264e
mansigupta <imansigupta29@gmail.com>
2018-03-13 17:26:27 -0400

    UI for ctegory and bug fix for shop page

---
+++ app/src/main/java/com/example/capstone/furniturestore/CategoryActivity.java
@@ -0,0 +1,237 @@
+package com.example.capstone.furniturestore;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.capstone.furniturestore.Class.Products;
+import com.example.capstone.furniturestore.Models.BlogHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    Intent intent;
+    final Context context = this;
+    public RecyclerView mBlogList;
+    FirebaseDatabase database;
+    private DatabaseReference prodDatabase, deptDatabase;
+    TextView textView;
+
+    MaterialSearchView searchView;
+
+    Toolbar toolbar;
+    ViewPager view_Pager;
+    Timer timer;
+    private int currentPage = 0;
+    ListView listview;
+
+    List<String> listOfString = new ArrayList<String>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+
+        prodDatabase = FirebaseDatabase.getInstance().getReference("Products");
+
+
+
+
+
+        //toolBar settings
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(1);
+        getSupportActionBar().setTitle(" supreme Furniture");
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        searchView = (MaterialSearchView) findViewById(R.id.search_view);
+
+
+        prodDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Integer i = 0;
+                for (DataSnapshot productSnapshot : dataSnapshot.getChildren()) {
+                    Products products = productSnapshot.getValue(Products.class);
+                    listOfString.add(products.getProductName());
+
+                    i++;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        listview = (ListView) findViewById(R.id.listview);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listOfString);
+        listview.setAdapter(adapter);
+
+
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                listview = (ListView) findViewById(R.id.listview);
+                ArrayAdapter adapter = new ArrayAdapter(CategoryActivity.this, android.R.layout.simple_list_item_1, listOfString);
+                listview.setAdapter(adapter);
+
+            }
+        });
+
+
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText != null && !newText.isEmpty()) {
+                    List<String> lstfound = new ArrayList<String>();
+                    for (String item : listOfString) {
+                        if (item.contains(newText))
+                            lstfound.add(item);
+                    }
+                    ArrayAdapter adapter = new ArrayAdapter(CategoryActivity.this, android.R.layout.simple_list_item_1, lstfound);
+                    listview.setAdapter(adapter);
+                } else {
+                    ArrayAdapter adapter = new ArrayAdapter(CategoryActivity.this, android.R.layout.simple_list_item_1, listOfString);
+                    listview.setAdapter(adapter);
+                }
+                return true;
+            }
+        });
+
+
+
+
+        //Recycler View
+
+        mBlogList = (RecyclerView) findViewById(R.id.blog_list);
+        mBlogList.setHasFixedSize(true);
+        mBlogList.setLayoutManager(new GridLayoutManager(this, 1));
+
+
+    }
+
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+
+
+        // Retrieve the SearchView and plug it into SearchManager
+    /*   final SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.search_view));
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));*/
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_basket) {
+            Intent intent = new Intent(getApplicationContext(), ShoppingBasketActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void onStart() {
+
+        //Send a query to the database
+        deptDatabase = FirebaseDatabase.getInstance().getReference("Department");
+
+        // database = FirebaseDatabase.getInstance();
+        //    mDatabase = database.getReference("Department");
+        deptDatabase.keepSynced(true);
+
+        super.onStart();
+
+        FirebaseRecyclerAdapter<BlogHolder, StoreActivity.BlogViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<BlogHolder, StoreActivity.BlogViewHolder>(BlogHolder.class, R.layout.blog_row, StoreActivity.BlogViewHolder.class, deptDatabase) {
+            @Override
+            protected void populateViewHolder(StoreActivity.BlogViewHolder viewHolder, BlogHolder model, int position) {
+                viewHolder.setTitle(model.getTitle());
+                viewHolder.setImage(getApplicationContext(), model.getImage());
+            }
+
+        };
+
+        mBlogList.setAdapter(firebaseRecyclerAdapter);
+
+    }
+
+    public static class BlogViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+
+        public BlogViewHolder(View itemView) {
+            super((itemView));
+            mView = itemView;
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+
+                }
+
+            });
+        }
+
+        public void setTitle(String title) {
+            TextView post_title = (TextView) mView.findViewById(R.id.title);
+            post_title.setText(title);
+
+        }
+
+        public void setImage(Context ctx, String image) {
+            ImageView post_image = (ImageView) mView.findViewById(R.id.post_image);
+            Picasso.with(ctx).load(image).into(post_image);
+        }
+    }
+
+}
